faf2e8211c09d6838bcf3e89ab7a54acfb19f2ee
Denis <denis.pptx@gmail.com>
2023-09-03 16:45:17 +0300

    all fields in registration are reuired

--- account/forms.py
+++ account/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class UserRegistrationForm(forms.ModelForm):
     username = forms.CharField(label='Username', help_text='', widget=forms.TextInput)
+    first_name = forms.CharField(label='First Name', required=True)
+    last_name = forms.CharField(label='Last Name', required=True)
+    email = forms.CharField(label='Email', widget=forms.EmailInput)
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
