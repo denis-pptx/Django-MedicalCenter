aafce2c37be4adf2e8f93ce894e07caf7f30b22e
Denis <denis.pptx@gmail.com>
2023-09-12 01:54:58 +0300

    news details pages

--- news/views.py
+++ news/views.py
@@ -4,9 +4,10 @@ from news.common import get_path_to_html
 
 
 def news(request):
-    return render(request, 'news/news.html', context={'news': News.objects.all()})
+    news_list = News.objects.order_by('-publish_date')
+    return render(request, 'news/news.html', context={'news': news_list})
 
 
 def news_details(request, pk):
     certain_news = get_object_or_404(News, pk=pk)
-    return render(request, get_path_to_html(certain_news), context={'news': news})
+    return render(request, get_path_to_html(certain_news), context={'news': certain_news})
