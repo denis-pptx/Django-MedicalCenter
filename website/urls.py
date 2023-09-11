68ca62f3793fad9c8355061aa448887907a54185
Denis <denis.pptx@gmail.com>
2023-09-11 13:23:55 +0300

    faq page

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,7 @@ from .views import *
 urlpatterns = [
     path('', TemplateView.as_view(template_name="website/home.html"), name='home'),
     path('about/', TemplateView.as_view(template_name="website/about.html"), name='about'),
+    path('faq/', TemplateView.as_view(template_name="website/faq.html"), name='faq'),
     path('weather/', get_weather, name='weather'),
     path('gender/', get_gender, name='gender'),
 ]
