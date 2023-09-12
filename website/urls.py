273c0577ce90801e070ced464e48d87a36c4d790
Denis <denis.pptx@gmail.com>
2023-09-12 19:56:32 +0300

    promo codes page

--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="website/about.html"), name='about'),
     path('faq/', TemplateView.as_view(template_name="website/faq.html"), name='faq'),
     path('private-policy/', TemplateView.as_view(template_name="website/private-policy.html"), name='private-policy'),
+    path('promo-codes/', TemplateView.as_view(template_name="website/promo-codes.html"), name='promo-codes'),
     path('weather/', get_weather, name='weather'),
     path('gender/', get_gender, name='gender'),
 ]
