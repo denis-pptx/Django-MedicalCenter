1e211d2a88087658f5fdc2fc21e3086a2007221a
Denis <denis.pptx@gmail.com>
2023-09-12 13:07:00 +0300

    added prvate-policy

--- website/urls.py
+++ website/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="website/home.html"), name='home'),
     path('about/', TemplateView.as_view(template_name="website/about.html"), name='about'),
     path('faq/', TemplateView.as_view(template_name="website/faq.html"), name='faq'),
+    path('private-policy/', TemplateView.as_view(template_name="website/private-policy.html"), name='private-policy'),
     path('weather/', get_weather, name='weather'),
     path('gender/', get_gender, name='gender'),
 ]
