44534d80233424a3778dbdea8bb113ec06b40580
Denis <denis.pptx@gmail.com>
2023-09-12 23:42:47 +0300

    added vacancies

--- website/urls.py
+++ website/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('faq/', TemplateView.as_view(template_name="website/faq.html"), name='faq'),
     path('private-policy/', TemplateView.as_view(template_name="website/private-policy.html"), name='private-policy'),
     path('promo-codes/', TemplateView.as_view(template_name="website/promo-codes.html"), name='promo-codes'),
+    path('vacancies/', TemplateView.as_view(template_name="website/vacancies.html"), name='vacancies'),
     path('feedback-list', feedback_list, name='feedback-list'),
     path('weather/', get_weather, name='weather'),
     path('gender/', get_gender, name='gender'),
