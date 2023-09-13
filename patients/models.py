da965ef67e5521e673b4cd32a1e0087915a34945
Denis <denis.pptx@gmail.com>
2023-09-14 00:51:53 +0300

    18+ patients

--- patients/models.py
+++ patients/models.py
@@ -1,15 +1,21 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
+from django.core.validators import RegexValidator
 from datetime import date
 from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+def validate_age(value):
+    today = date.today()
+    age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+    if age < 18 or age > 130:
+        raise ValidationError('Возраст должен быть от 18 до 130 лет')
+
+
 class PatientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(
-        validators=[MinValueValidator(date(1900, 1, 1)), MaxValueValidator(date.today())]
-    )
+    date_of_birth = models.DateField(validators=[validate_age])
     address = models.CharField(max_length=100)
 
     phone_number = models.CharField(
