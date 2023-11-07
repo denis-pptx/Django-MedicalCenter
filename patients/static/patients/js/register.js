472e354adb1623e289e33d8b916b669e7eaa24db
Denis-PC <denis.pptx@gmail.com>
2023-11-07 23:17:58 +0300

    18+ registration

---
+++ patients/static/patients/js/register.js
@@ -0,0 +1,34 @@
+let dateInput = document.getElementById('id_date_of_birth');
+dateInput.setAttribute('max', new Date().toISOString().slice(0, 10));
+dateInput.setAttribute('min', '1900-01-01');
+
+let info = document.querySelector('.info');
+let registerButton = document.getElementById('register');
+
+const daysOfWeek = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+
+dateInput.addEventListener('blur', function () {
+    setTimeout(function () {
+        let birthDate = new Date(dateInput.value);
+        let currentDate = new Date();
+
+        let ageInMilliseconds = currentDate - birthDate;
+        let years = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365));
+
+        let dayOfWeek = birthDate.getDay();
+        let text = `Дата рождения: ${dateInput.value} (${daysOfWeek[dayOfWeek]})<br>Возраст: ${years}`;
+
+        if (years < 18) {
+            alert('Малышам сюда нельзя!')
+            text += '<br>Позови старших!'
+            registerButton.disabled = true;
+        }
+        else {
+            registerButton.disabled = false;
+        }
+
+        info.innerHTML = text;
+
+
+    }, 0);
+});
