dbfc6a48e893332ed4123666b50baf197e68e326
Denis-HP <denis.pptx@gmail.com>
2023-11-08 12:35:29 +0300

    add row button

--- website/static/website/js/magic-table.js
+++ website/static/website/js/magic-table.js
@@ -1,9 +1,15 @@
 let table = document.getElementById('magic-table');
+
 const sizeInput = document.getElementById('size');
 const createButton = document.getElementById('create');
+
 const transposeButton = document.getElementById('transpose');
 
-createButton.addEventListener('click', function () {
+const addRowButton = document.getElementById('addRow');
+const addColumnButton = document.getElementById('addColumn');
+
+
+function createTable() {
     let size = sizeInput.value;
     if (size < 1 || size > 20) {
         alert('Размер таблицы должен быть от 1 до 20.');
@@ -21,18 +27,18 @@ createButton.addEventListener('click', function () {
             cell.textContent = Math.floor(Math.random() * 1000);
         }
     }
-});
+}
 
-transposeButton.addEventListener('click', function () {
+function transposeTable() {
     let rows = table.rows;
     let cols = rows[0].cells;
 
     if (rows.length == 0) 
         return; // Таблица пустая
-    
+
     // Создать новую таблицу для транспонирования
     let transposedTable = table.cloneNode();
-    
+
     for (let i = 0; i < cols.length; i++) {
         let transposedRow = transposedTable.insertRow(i);
         for (let j = 0; j < rows.length; j++) {
@@ -44,4 +50,18 @@ transposeButton.addEventListener('click', function () {
     // Заменить таблицу
     table.replaceWith(transposedTable)
     table = transposedTable;
-});
+}
+
+function addRow() {
+    let newRow = table.insertRow(table.rows.length);
+    let cols = table.rows[0].cells;
+    for (let i = 0; i < cols.length; i++) {
+        let cell = newRow.insertCell(i);
+        cell.textContent = Math.floor(Math.random() * 1000);
+    }
+    transposeButton.disabled = false;
+}
+
+createButton.addEventListener('click', createTable);
+transposeButton.addEventListener('click', transposeTable);
+addRowButton.addEventListener('click', addRow);
